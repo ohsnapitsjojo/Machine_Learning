8880fd88a82a59f6ec741b358e07f8605c5f8efe
ohsnapitsjojo <j.truongle@hotmail.com>
2016-05-27 14:42:54 +0200

    exe2 test novel v2

--- Assignment1/Exercise2/exercise2.m
+++ Assignment1/Exercise2/exercise2.m
@@ -1,4 +1,6 @@
 %% Load parameter
+clear all;
+tic;
 
 d = 500;
 
@@ -6,31 +8,59 @@ d = 500;
 
 images = loadMNISTImages('train-images.idx3-ubyte'); 
 labels = loadMNISTLabels('train-labels.idx1-ubyte');
+test_images = loadMNISTImages('t10k-images.idx3-ubyte');
+test_labels = loadMNISTLabels('t10k-labels.idx1-ubyte');
 
 %% PCA
 
-mean = mean2(images);                       % Compute the mean
-tmp_images = images-ones(size(images))*mean;% Make the data zero-mean
+means = mean2(images);                       % Compute the mean
+tmp_images = images - means;% Make the data zero-mean
 S = cov(tmp_images.');                      % Compute Cov Matrix
 [V, D] = eig(S);                            % Eigenvectors and Eigenvalues
     
-D = sum(D);     
+D = diag(D);     
 [B, I] = sort(D);
 I = fliplr(I);
 basis = V(:,I(1:d));                            % Chose the strongest Eigenvectors
 
 y = basis.'*images;                             % Project the data on these basis
 
-%% Computation the man and covaraicne of each digit class seperately
+%% Computation of the mean and covaraicne of each digit class seperately
 
-mean_vector = sum(images)/size(images,1);   % calculate mean vector for each image
-cov_vector = cov(images(:,1));
+class = cell(10,1);
+mean_class = cell(10,1);
+cov_class = cell(10,1);
+proj_class = cell(10,1);
+l_class = cell(10,1);
 
-for i=1:size(images,1)                      % Substract mean vector from input data
-    tmp_images(i,:) = tmp_images(i,:) - mean_vector;
+mean_vector = zeros(1,9);
+tmp_sum = mean(images);
+cov_vector = zeros(1,9);
+
+for i=0:9
+
+    
+    tmp_ind = find(test_labels == i);
+    tmp_mean = tmp_sum(:,tmp_ind);
+    mean_class{i+1} = mean(tmp_mean);
+    class{i+1} = test_images(:,tmp_ind) - ones(size(test_images,1),1)*tmp_mean;
+    cov_class{i+1} = cov(class{i+1}'); 
+    proj_class{i+1} = basis.'*class{i+1};
+    
 end
 
-y_neu = basis.'*tmp_images;                      % Project Data on already learned basis
+
+
+
+toc;
+
+% cov_vector = cov(y(:,1));
+% 
+% for i=1:size(y,1)                      % Substract mean vector from input data
+%     tmp_images(i,:) = tmp_images(i,:) - mean_vector;
+% end
+% 
+% y_neu = basis.'*tmp_images;                      % Project Data on already learned basis
 
 
 
