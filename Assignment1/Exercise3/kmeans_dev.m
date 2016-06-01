b17d18f4632a0380d1910f3b2167f4a32c9ed5e5
ohsnapitsjojo <j.truongle@hotmail.com>
2016-06-01 13:04:03 +0200

    exe 3, kovergiert

--- Assignment1/Exercise3/kmeans_dev.m
+++ Assignment1/Exercise3/kmeans_dev.m
@@ -10,6 +10,7 @@ threshold = 10^-6;
 %% k means
 
 dod = 420;
+d = 420;
 euk_distance = zeros(size(input, 1), k);
 class = cell(7,1);
 
@@ -18,16 +19,16 @@ while dod > threshold
     euk_distance = pdist2(input, means);
     [min_d , matched_class] = min(euk_distance' ,[], 1);
     
-    tmp_dod = 0;
+    tmp_d = 0;
     for i=1:k
         tmp_ind = find(matched_class() == i);
         class{i} = input(tmp_ind,:);
-        tmp_dod = tmp_dod + sum(pdist2(class{i}, means(i,:)));
+        tmp_d = tmp_d + sum(pdist2(class{i}, means(i,:)));
         if size(class{i}) ~= 0    
             means(i,:) = mean(class{i});
         end
         
     end
-    dod = abs(dod - tmp_dod)
-    
+    dod = abs(d - tmp_d);
+    d = tmp_d;
 end
